93850ff302aea586eb1ae40d71f4a6a7facf7d36
张启卫 <david@zhangqiweideMacBook-Pro.local>
2019-01-26 15:22:46 +0800

    Test

--- start.py
+++ start.py
@@ -38,7 +38,8 @@ def device_registry():
     d = bytes.fromhex('55 AA C0 02 FF FF')
     s.write(d)
 
-    reading = ser.read(6)
+    reading = s.read(6)
+    reading_hex = bytes.fromhex(reading)
     # return Code 57 AB C0 01 00
     # 57 AB stands for upload fix head
     # C0 code stands for set default frequency
@@ -47,7 +48,7 @@ def device_registry():
     # FF code stands frequence Failed
 
     s.close()
-    return reading
+    return reading_hex
 
 
 

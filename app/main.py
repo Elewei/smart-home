e50d31bc26994a3a81293bbf9b4351c5c3cc18e8
Zhang Qiwei <mzhangqiwei@hotmail.com>
2019-02-20 23:39:04 +0800

    Test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import get_db
+from app.chinatime import this_month
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
+
+    this_month()
     return render_template('index.html')

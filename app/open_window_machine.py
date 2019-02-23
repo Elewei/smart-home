ad1bdfa848aae4d191a51ec8b2d3fb40db2328ca
Zhang Qiwei <mzhangqiwei@hotmail.com>
2019-02-23 22:39:14 +0800

    Test

--- app/open_window_machine.py
+++ app/open_window_machine.py
@@ -258,47 +258,6 @@ def close_window_macine_full():
 
 
 
-
-@bp.route('/openwindowfull')
-def open_window_macine_full():
-
-    print("开窗器到 100%")
-    openWindowAddress = request.args.get('openWinowMachineAddress', 0, type=int)
-    print(openWindowAddress)
-
-    #Device Register
-    # Step 1 Open the serial port
-    ser = serial.Serial('/dev/ttyAMA0',230400)
-    ser.timeout = 3
-
-    # Device Register "01 01 bb 10 30 64 00"
-    # 01 01 code Stands for LoraID Address
-    # bb code stands for control code
-    # 10 code stands for Open Window Machine Type
-    # 30 code stands for Open Window Machine Address
-    # 64 open window percent
-    message_send = "01 01 bb 10 "+ str(openWindowAddress) +" 64"
-    print("发送消息" + message_send)
-    message_send_hex = bytes.fromhex(message_send)
-    ser.write(message_send_hex)
-
-    reading = ser.read(6)
-    reading_str = ''.join(['%02x ' % b for b in reading])
-    # return Code 01 01 bb 10 31 01
-    # 01 01 stands for upload fix head
-    # bb code stands for control code
-    # 10 code stands Open Window Machine
-    # 31 code stands for Open Window Address
-    # 01 code stands Success
-    print("第一次收到消息 = " + reading_str)
-
-
-    ser.close()
-    return jsonify(result=1)
-
-
-
-
 @bp.route('/stopwindow')
 def stop_window_macine():
 

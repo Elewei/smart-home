29890c23c6745b6bbb15f62bf05925af3e77fa7b
Zhang Qiwei <mzhangqiwei@hotmail.com>
2019-02-24 02:32:30 +0800

    Test

--- app/curtain.py
+++ app/curtain.py
@@ -108,7 +108,7 @@ def device_registry():
     else:
         return jsonify(result=0)
 
-    global DEVICE_ADDRESS += 1
+    DEVICE_ADDRESS += 1
     # 修改通信频率 '55 AA C1 02 01 01'
     # 55 AA 表示固定首部
     # C1 表示设置定义频率

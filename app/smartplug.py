08938a93fd5bc7637c14b123346b142750a55a84
elewei <mzhangqiwei@hotmail.com>
2019-02-26 14:09:04 +0800

    Test

--- app/smartplug.py
+++ app/smartplug.py
@@ -9,7 +9,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import get_db
 from . import packet
 
-DEVICE_ADDRESS = 100
+DEVICE_ADDRESS = 60
 
 # 创建一个 blueprint
 bp = Blueprint('smartplug', __name__, url_prefix='/smartplug')
